1a96b505b07960c8e01ebebedf3170b98220cfee
nakahashi kentaro <nakahashi_k@uuum.jp>
2017-07-12 02:31:45 +0900

    deployer

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 
 git config --global user.email "${GIT_USER_EMAIL}"
 git config --global user.name "${GIT_USER_NAME}"
+git checkout gh-pages
 git add index.html
 git commit -m "deploy [ci skip]"
 git pull git@github.com:nakahashi/study-api-blueprint.git gh-pages
-git push git@github.com:nakahashi/study-api-blueprint.git gh-pages
+git push git@github.com:nakahashi/study-api-blueprint.git gh-pages -f
